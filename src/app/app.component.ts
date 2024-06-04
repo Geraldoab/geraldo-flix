ed0419360104eeba1cc4c07aab0c403562f3cb90
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2024-06-03 21:28:56 -0300

    add a picture profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PictureProfileComponent } from './components/profile/picture-profile/picture-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PictureProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
