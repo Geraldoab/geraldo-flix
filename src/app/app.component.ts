0a96de80cc141760f43b9c27186ab754a14bfc7a
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2024-06-09 18:01:16 -0300

    add a custom-button to the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PictureProfileComponent } from './components/profile/picture-profile/picture-profile.component';
 import { ManageProfileComponent } from './components/profile/manage-profile/manage-profile.component';
+import { ButtonComponent } from './components/core/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PictureProfileComponent, ManageProfileComponent],
+  imports: [RouterOutlet, PictureProfileComponent, ManageProfileComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
